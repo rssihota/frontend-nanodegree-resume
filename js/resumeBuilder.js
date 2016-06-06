dd54256f1a49d874d5cb6851fafb51209dfe1e66
Ranbir Sihota <ranbirsihota@gmail.com>
2016-06-06 17:03:55 -0400

    Add name and role

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,4 +2,19 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
- $("#main").append("Ranbir Sihota");
+/*var awesomeThoughts = "I am Ranbir and I am AWESOME!";
+console.log(awesomeThoughts);
+
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+console.log(funThoughts);
+
+$("#main").append(funThoughts);*/
+
+var name = "Ranbir Sihota";
+var role = "Software Developer";
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
